4c4de9a2c111935c4e1c1931871810d6854e80a0
jordi.peralta <jordi.peraltadolz@ingrammicro.com>
2021-07-27 18:41:00 +0200

    LITE-19730: Missing Capabilities

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/extension.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_slug}}/extension.py
@@ -15,8 +15,9 @@ from connect.eaas.extension import (
         cookiecutter.subscription_process_capabilities_4of6 == 'y' or 
         cookiecutter.subscription_process_capabilities_5of6 == 'y' or 
         cookiecutter.subscription_process_capabilities_6of6 == 'y' or
-        cookiecutter.tier_config_process_capabilities_1of2 == 'y' or 
-        cookiecutter.tier_config_process_capabilities_2of2 == 'y') %}
+        cookiecutter.tier_config_process_capabilities_1of3 == 'y' or 
+        cookiecutter.tier_config_process_capabilities_2of3 == 'y' or
+        cookiecutter.tier_config_process_capabilities_3of3 == 'y') %}
     ProcessingResponse,
     {%- endif %}
     {%- if cookiecutter.product_capabilities_1of2 == 'y' %}
@@ -76,15 +77,22 @@ class {{ cookiecutter.project_name|title|replace(" ", "") }}Extension(Extension)
 
     {% endif -%}
 
-    {% if cookiecutter.tier_config_process_capabilities_1of2 == 'y' -%}
+    {% if cookiecutter.tier_config_process_capabilities_1of3 == 'y' -%}
     {% if cookiecutter.use_asyncio == 'y' %}async {% endif %}def process_tier_config_setup_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
         return ProcessingResponse.done()
 
     {% endif -%}
 
-    {% if cookiecutter.tier_config_process_capabilities_2of2 == 'y' -%}
+    {% if cookiecutter.tier_config_process_capabilities_2of3 == 'y' -%}
     {% if cookiecutter.use_asyncio == 'y' %}async {% endif %}def process_tier_config_change_request(self, request):
+        self.logger.info(f"Obtained request with id {request['id']}")
+        return ProcessingResponse.done()
+
+    {% endif -%}
+
+    {% if cookiecutter.tier_config_process_capabilities_3of3 == 'y' -%}
+    {% if cookiecutter.use_asyncio == 'y' %}async {% endif %}def process_tier_config_adjustment_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
         return ProcessingResponse.done()
 
